0634fdffc55648d84a610b1f2292f2614fa59126
nguyen378 <82461544+nguyen378@users.noreply.github.com>
2023-05-22 22:23:17 +0700

    int project

---
+++ DoAn-Java-Chess/src/main/java/com/donutnv/doan/java/chess/DoAnJavaChess.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.donutnv.doan.java.chess;
+
+/**
+ *
+ * @author nguye
+ */
+public class DoAnJavaChess {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
